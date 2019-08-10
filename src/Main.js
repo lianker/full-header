5209a50469ddf6198dd6f9aaa1575d89ef789740
lianker lopes <lianker.dev@gmail.com>
2019-08-10 01:18:30 -0300

    add many things

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { headerStyle, containerStyle, titleStyle, subtitleStyle, videoStyle } from './Styles';
 
-const FullHeader = ({ title }) => (
-    <h1>Hello {title}!</h1>
-);
+const defaultProps = {
+    bgColor: '#ccc',
+    textColor: '#fff',
+    font: 'sans-serif',
+    bgImage: '',
+};
+
+const FullHeader = ({ title, subtitle, bgColor, textColor, font, bgImage, video }) => {
+    const headerStyleCombined = {
+        ...headerStyle,
+        backgroundColor: bgColor,
+        color: textColor,
+        fontFamily: font,
+        backgroundImage: `url(${bgImage})`,
+    };
+    const component = (
+        <header style={headerStyleCombined}>
+            <div style={containerStyle}>
+                {title && <h1 style={titleStyle}>{title}</h1>}
+                {subtitle && <h2 style={subtitleStyle}>{subtitle}</h2>}
+            </div>
+            {video && <video style={videoStyle} src={video} autoPlay muted loop />}
+        </header>
+    );
+
+    return component;
+};
 
 FullHeader.propTypes = {
-    title: PropTypes.string.isRequired,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
+    font: PropTypes.string,
+    bgImage: PropTypes.string,
+    video: PropTypes.string,
 };
+
+FullHeader.defaultProps = defaultProps;
 
 export default FullHeader;
--- stories/Main.js
+++ stories/Main.js
@@ -4,23 +4,48 @@ import { storiesOf } from '@storybook/react';
 
 storiesOf('Full header', module)
     .add('with title', () => (
-        <FullHeader title="TDD" />
+        <FullHeader title="Cerodonia" />
     ))
     .add('with title ,subtitle', () => (
-        <FullHeader title="TDD" subtitle="teste" />
+        <FullHeader title="Cerodonia"
+            subtitle="Um mundo magico habitado por feras"
+        />
     ))
     .add('with title ,subtitle, bgColor', () => (
-        <FullHeader title="TDD" subtitle="teste" bgColor="#ccc" />
+        <FullHeader
+            title="Cerodonia"
+            subtitle="Um mundo magico habitado por feras"
+            bgColor="#0000FF"
+        />
     ))
     .add('with title ,subtitle, bgColor and text color', () => (
-        <FullHeader title="TDD" subtitle="teste" bgColor="#ccc" textColor="#555" />
+        <FullHeader
+            title="Cerodonia"
+            subtitle="Um mundo magico habitado por feras"
+            bgColor="#0000FF"
+            textColor="#00FF00"
+        />
     ))
     .add('with title ,subtitle, bgColor, text color and font', () => (
-        <FullHeader title="TDD" subtitle="teste" bgColor="#ccc" textColor="#555" font="verdana" />
+        <FullHeader
+            title="Cerodonia"
+            subtitle="Um mundo magico habitado por feras"
+            bgColor="#FFF"
+            textColor="#00FF00"
+            font="Times New Roman"
+        />
     ))
     .add('with title ,subtitle, bgImage', () => (
-        <FullHeader title="TDD" subtitle="teste" bgImage="img" />
+        <FullHeader
+            title="Cerodonia"
+            subtitle="Um mundo magico habitado por feras"
+            bgImage="https://www.miaonline.org/wp-content/uploads/red-background-and-photo-high-resolution-download.jpg"
+        />
     ))
     .add('with title ,subtitle, video', () => (
-        <FullHeader title="TDD" subtitle="teste" video="img" />
+        <FullHeader
+            title="Cerodonia"
+            subtitle="Um mundo magico habitado por feras"
+            video="https://static.videezy.com/system/resources/previews/000/005/013/original/Golden_Nebula_4K_Motion_Background.mp4"
+        />
     ));
